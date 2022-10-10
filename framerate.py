f9058a326710d349e0a9ebb3920d0cc12c02da98
llopen-sourcell <50943349+llopen-sourcell@users.noreply.github.com>
2022-10-10 17:39:37 +0530

    Update framerate.py

--- framerate.py
+++ framerate.py
@@ -1,18 +1,30 @@
+from asyncore import write
 import math
+import serial
 import cv2
+import time
 from cvzone import ColorFinder,findContours
 import numpy as np
-#from SerComm import write_read
-cap = cv2.VideoCapture(0)
+
+cap = cv2.VideoCapture(1)
 cap.set(3,620)
 cap.set(4,480)
 myColorFinder = ColorFinder(False)
-
+arduino = serial.Serial(port='COM7', baudrate=115200, timeout=.1)
 #hsvVals={'hmin': 6, 'smin': 101, 'vmin': 126, 'hmax': 13, 'smax': 234, 'vmax': 255}
-hsvVals={'hmin': 27, 'smin': 35, 'vmin': 153, 'hmax': 43, 'smax': 255, 'vmax': 255}
+hsvVals={'hmin': 11, 'smin': 56, 'vmin': 234, 'hmax': 43, 'smax': 237, 'vmax': 255}
 posListX,posListY = [],[]
 xList = [item for item in range(1280)]
 depthh=0
+flag=True
+angleList=[]
+
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+
 while True:
     success,img = cap.read()
     #img = img[:, 500:]
@@ -26,12 +38,13 @@ while True:
         dx,dy=cx,cy 
         cy = np.abs(cy-480)
         
-        print(cx,cy)
-        depthh = 31.2618 - 0.000446406* int(contours[0]['area'])
+        #print(int(contours[0]['area']))
+        depthh = -0.002249*int(contours[0]['area']) + 46.72
         cv2.putText(imgContours,'Depth= '+str(int(depthh)),(30,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2,cv2.LINE_AA)
         cv2.putText(imgContours,'X = '+str(int(cx)),(30,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2,cv2.LINE_AA)
         cv2.putText(imgContours,'Y = '+str(int(cy)),(30,90),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2,cv2.LINE_AA)
         if depthh < 25:
+            
             cv2.line(imgContours, (310, 480), (dx, dy), (0, 255, 0), thickness=2)
             if dx==310:
                 pass
@@ -41,7 +54,11 @@ while True:
             angle = math.degrees(math.atan(slope_m))
             if angle < 0:
                 angle+=180
-
+            angleList.append(angle)
+            if len(angleList) > 5 and flag == True:
+                write_read(str(math.ceil(angle)))
+                #print(math.ceil(angle))
+                flag=False
             cv2.putText(imgContours,'angle'+str(int(angle)),(cx+10,dy+10),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2,cv2.LINE_AA)
     if posListX:
         
@@ -55,4 +72,4 @@ while True:
     if cv2.waitKey(33) == ord('a'):
         posListX,posListY = [],[]           
     if cv2.waitKey(1) == ord('p'):
-        cv2.waitKey(-1) #wait until any key is pressed
+        flag=True
